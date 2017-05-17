137bdf632c42b312c3805644996a44f0ed2d1b2b
bin <bin@ngs.tech>
2017-05-17 14:29:16 +0800

    update

--- ch-13/issues/lib/issues/cli.ex
+++ ch-13/issues/lib/issues/cli.ex
@@ -47,10 +47,12 @@ defmodule Issues.CLI do
         System.halt(0)
     end
 
-    def process({user, project, _count}) do 
+    def process({user, project, count}) do 
         Issues.GithubIssues.fetch(user, project)
         |> decode_response
         |> sort_into_ascending_order
+        |> Enum.take(count)
+        |> Issues.TableFormatter.print_table_for_columns(["number", "created_at", "title"])
     end
 
     def decode_response({:ok, body}), do: body
