599216e74a9234ed9f76c324227254cf3e3d24d0
bin <bin@ngs.tech>
2017-05-19 18:04:40 +0800

    update

--- ch-13/issues/mix.exs
+++ ch-13/issues/mix.exs
@@ -30,10 +30,10 @@ defmodule Issues.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      httpoison: "~> 0.11.2",
-      poison: "~> 3.0"
+      {:httpoison,  "~> 0.11.2"},
+      {:poison,     "~> 3.0"},
+      {:ex_doc,     "~> 0.14"}
     ]
-
   end
 
   defp escript_config do
